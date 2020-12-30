71f3b31cfafd9b6e50092ee0fd241e48f1b5221b
nhnghia <nghianguyenhuu848@gmail.com>
2020-12-30 13:45:01 +0700

    display greeting

---
+++ Module 2/01-Introduction in Java/src/ex01_hien_thi_loi_chao/HienThiLoiChao.java
@@ -0,0 +1,14 @@
+package ex01_hien_thi_loi_chao;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter your name:");
+		String name = keyboard.nextLine();
+		System.out.println("Hello: " + name);
+	}
+
+}
