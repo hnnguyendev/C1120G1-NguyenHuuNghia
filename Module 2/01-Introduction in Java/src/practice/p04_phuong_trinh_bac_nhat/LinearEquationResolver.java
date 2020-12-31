db7c8ac55127bce0b6d5e8cc4feebbe8c2856285
nhnghia <nghianguyenhuu848@gmail.com>
2020-12-31 11:10:31 +0700

    practice introduction in java

---
+++ Module 2/01-Introduction in Java/src/practice/p04_phuong_trinh_bac_nhat/LinearEquationResolver.java
@@ -0,0 +1,37 @@
+package practice.p04_phuong_trinh_bac_nhat;
+
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+
+	static double a;
+	static double b;
+
+	static void input() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("a: ");
+		a = scanner.nextDouble();
+		System.out.print("b: ");
+		b = scanner.nextDouble();
+	}
+
+	static void solveLinearEquation() {
+		System.out.println("Linear Equation Resolver");
+		System.out.println("Given a equation as 'a * x + b = 0', please enter constants:");
+		input();
+		if (a != 0) {
+			double solution = -b / a;
+			System.out.printf("Equation pass with x = %.1f!", solution);
+		} else if (b == 0) {
+			System.out.println("The solution is all x");
+		} else {
+			System.out.println("No solution!");
+		}
+	}
+
+	public static void main(String[] args) {
+		solveLinearEquation();
+
+	}
+
+}
---
+++ Module 2/01-Introduction in Java/src/practice/p06_kiem_tra_nam_nhuan/LeapYear.java
@@ -0,0 +1,32 @@
+package practice.p06_kiem_tra_nam_nhuan;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	static int year;
+
+	static void input() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the year you want to check:");
+		year = scanner.nextInt();
+	}
+
+	static boolean isLeapYear(int year) {
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		input();
+		if (isLeapYear(year)) {
+			System.out.println(year + " is a leap year");
+		} else {
+			System.out.println(year + " is NOT a leap year");
+		}
+
+	}
+
+}
