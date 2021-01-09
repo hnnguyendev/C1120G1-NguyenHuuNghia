561c7e7c372fe84647b77bea34173410832cf7f4
nhnghia <nghianguyenhuu848@gmail.com>
2021-01-09 15:51:47 +0700

    point and moveablepoint

---
+++ Module 2/06-Inheritance/src/exercise/ex03_lop_point_va_moveablepoint/MoveablePoint.java
@@ -0,0 +1,63 @@
+package exercise.ex03_lop_point_va_moveablepoint;
+
+import java.util.Arrays;
+
+public class MoveablePoint extends Point {
+
+	private float xSpeed;
+	private float ySpeed;
+
+	public MoveablePoint() {
+		xSpeed = 0f;
+		ySpeed = 0f;
+	}
+
+	public MoveablePoint(float xSpeed, float ySpeed) {
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+	}
+
+	public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+		super(x, y);
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+	}
+
+	public float getxSpeed() {
+		return xSpeed;
+	}
+
+	public void setxSpeed(float xSpeed) {
+		this.xSpeed = xSpeed;
+	}
+
+	public float getySpeed() {
+		return ySpeed;
+	}
+
+	public void setySpeed(float ySpeed) {
+		this.ySpeed = ySpeed;
+	}
+
+	public void setSpeed(float xSpeed, float ySpeed) {
+		this.xSpeed = xSpeed;
+		this.ySpeed = ySpeed;
+	}
+
+	public float[] getSpeed() {
+		return new float[] { this.xSpeed, this.ySpeed };
+	}
+
+	@Override
+	public String toString() {
+		return "MoveablePoint [xSpeed=" + xSpeed + ", ySpeed=" + ySpeed + ", getSpeed()=" + Arrays.toString(getSpeed())
+				+ ", getX()=" + getX() + ", getY()=" + getY() + ", getXY()=" + Arrays.toString(getXY()) + "]";
+	}
+
+	public MoveablePoint move() {
+		setX(getX() + this.xSpeed);
+		setY(getY() + this.ySpeed);
+		return this;
+	}
+
+}
---
+++ Module 2/06-Inheritance/src/exercise/ex03_lop_point_va_moveablepoint/TestMoveablePoint.java
@@ -0,0 +1,21 @@
+package exercise.ex03_lop_point_va_moveablepoint;
+
+public class TestMoveablePoint {
+
+	public static void main(String[] args) {
+		Point point = new Point(3, 5);
+		System.out.println(point);
+
+		MoveablePoint moveablePoint = new MoveablePoint(4, 4);
+		System.out.println(moveablePoint);
+
+		moveablePoint.setX(point.getX());
+		moveablePoint.setY(point.getY());
+
+		moveablePoint.move();
+		
+		System.out.println(moveablePoint);
+
+	}
+
+}
