80cc9e36a5e00ef86c026f14ad6972bf7120dfc1
nhnghia <nghianguyenhuu848@gmail.com>
2021-05-05 21:12:07 +0700

    typescript

--- Module 5/ss02-Typescript/exercise/typescript-fibonacci/src/main.js
+++ Module 5/ss02-Typescript/exercise/typescript-fibonacci/src/main.js
@@ -1,14 +1,34 @@
-function fibonacci() {
+function fibonacci1() {
     var f0 = 0;
     var f1 = 1;
+    console.log(f0);
+    console.log(f1);
     var sum = f0 + f1;
     var fn;
-    for (var index = 0; index < 9; index++) {
+    for (var index = 0; index < 8; index++) {
         fn = f0 + f1;
         f0 = f1;
         f1 = fn;
+        console.log(fn);
         sum += fn;
     }
     console.log("Sum = " + sum);
 }
-fibonacci();
+fibonacci1();
+function fibonacci(anyNumber) {
+    if (anyNumber <= 1) {
+        return anyNumber;
+    }
+    else {
+        return fibonacci(anyNumber - 1) + fibonacci(anyNumber - 2);
+    }
+}
+var n = 10;
+var fib;
+var sum = 0;
+for (var i = 0; i < n; i++) {
+    fib = fibonacci(i);
+    console.log(fib);
+    sum += fib;
+}
+console.log("Sum of Fibonacci = " + sum);
--- Module 5/ss02-Typescript/exercise/typescript-fibonacci/src/main.ts
+++ Module 5/ss02-Typescript/exercise/typescript-fibonacci/src/main.ts
@@ -1,15 +1,37 @@
-function fibonacci() {
+function fibonacci1() {
     let f0: number = 0;
     let f1: number = 1;
+    console.log(f0);
+    console.log(f1);
     let sum: number = f0 + f1;
     let fn: number;
     for (let index = 0; index < 8; index++) {
         fn = f0 + f1;
         f0 = f1;
         f1 = fn;
+        console.log(fn);
         sum += fn;
     }
     console.log(`Sum = ${sum}`);
 }
 
-fibonacci();
+fibonacci1();
+
+function fibonacci(anyNumber: number): number {
+    if (anyNumber <= 1) {
+        return anyNumber;
+    } else {
+        return fibonacci(anyNumber - 1) + fibonacci(anyNumber - 2);
+    }
+}
+
+let n: number = 10;
+let fib: number;
+let sum: number = 0;
+for (let i = 0; i < n; i++) {
+    fib = fibonacci(i);
+    console.log(fib);
+    sum += fib;
+}
+
+console.log(`Sum of Fibonacci = ${sum}`);
